f33ce112d316aa6d2a3d6b660d8e367810f9569d
alex653 <sn8@bk.ru>
2018-08-19 11:07:48 +0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-1
+var http = require('http');
+
+http.createServer(function (req, res) {
+    
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('Hello, world1!');
+    
+}).listen(process.env.PORT || 8080);
